61e9b76e32b92ddd022d19dbb6714ee55a3f6fad
Risqi Khasani <mrisqikhasani@gmail.com>
2022-08-28 12:57:07 +0700

    First Commit initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Helllo world welcome back again
+    </div>
+  );
+}
+
+export default App;
